f5b9577d59e53b50053b365fc79792b9afc9f9b8
Kaan TENİK <kaantenik@gmail.com>
2024-07-09 13:41:37 +0300

    Projeye Rolify Gem'i eklendi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
+  rolify
+  rolify :before_add => :before_add_method
+
+  def before_add_method(role)
+    # do something before it gets added
+  end
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
 end
